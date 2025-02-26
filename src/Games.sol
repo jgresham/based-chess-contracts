5fbed087f071bedde75e92f664cd3d994f047415
jgresham <johnsgresham@gmail.com>
2025-02-25 16:04:22 -0800

    verifyGameUpdate with result and winner

--- src/Games.sol
+++ src/Games.sol
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 
+uint8 constant NO_RESULT = 0;
+uint8 constant WINNING_RESULT = 1;
+uint8 constant DRAW_RESULT_AGREEMENT = 2;
+uint8 constant DRAW_RESULT_STALEMATE = 3;
+uint8 constant DRAW_RESULT_THREEFOLD_REPETITION = 4;
+
 struct GameUpdate {
   bytes signature;
   address signer;
@@ -11,6 +17,8 @@ struct Game {
   address creator;
   address player1;
   address player2;
+  uint8 result;
+  address winner;
   GameUpdate[] updates;
   // 3rd party verifications of (game state) updates
   mapping(address => uint8[]) updateVerifications;
@@ -20,7 +28,9 @@ contract Games {
   Game[] games;
 
   event GameCreated(uint256 indexed gameId);
-  event GameUpdateSynced(uint256 indexed gameId, string message, bytes signature, address signer);
+  event GameUpdateSynced(
+    uint256 indexed gameId, string message, bytes signature, address signer, uint8 updateIndex
+  );
 
   function createGame(address player1, address player2) public returns (uint256) {
     require(player1 != address(0) && player2 != address(0), "Player addresses cannot be 0");
@@ -51,20 +61,45 @@ contract Games {
     );
     // We can verify the signature offchain when verifyGameUpdate is called
     games[id].updates.push(GameUpdate({ signature: signature, signer: signer }));
-    emit GameUpdateSynced(id, message, signature, signer);
+    // if a player is spamming more than 225 updates, then they will be marked as spamming
+    emit GameUpdateSynced(id, message, signature, signer, uint8(games[id].updates.length - 1));
 
     return id;
   }
 
-  // anyone can verify a game update
-  function verifyGameUpdate(uint256 id, uint8 updateVerified) public {
+  // anyone can verify a game update, only the creator can set the result and winner
+  function verifyGameUpdate(uint256 id, uint8 updateVerified, uint8 result, address winner) public {
     require(id < games.length, "Game does not exist");
+    require(updateVerified <= games[id].updates.length, "Update does not exist");
     games[id].updateVerifications[msg.sender].push(updateVerified);
+    if (msg.sender == games[id].creator) {
+      if (result != 0) {
+        games[id].result = result;
+      }
+      if (winner != address(0) && result == WINNING_RESULT) {
+        require(
+          winner == games[id].player1 || winner == games[id].player2, "Winner must be a player"
+        );
+        games[id].winner = winner;
+      }
+    }
   }
 
-  function getGame(uint256 id) public view returns (uint256, GameUpdate[] memory, address, address) {
+  function getGame(uint256 id)
+    public
+    view
+    returns (uint256, address, address, address, uint8, address, GameUpdate[] memory)
+  {
     require(id < games.length, "Game does not exist");
-    return (games[id].id, games[id].updates, games[id].player1, games[id].player2);
+    return (
+      games[id].id,
+      games[id].creator,
+      games[id].player1,
+      games[id].player2,
+      games[id].result,
+      games[id].winner,
+      games[id].updates
+    );
   }
 
   function getGameUpdateVerificationsByVerifier(uint256 id, address verifier)
--- test/Games.t.sol
+++ test/Games.t.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.24;
 import { Test, console } from "forge-std/Test.sol";
 import { Games, GameUpdate } from "../src/Games.sol";
 
+address constant CREATOR = address(0x9999);
 address constant PLAYER1 = address(0x1234);
 address constant PLAYER2 = address(0x5678);
 
@@ -20,20 +21,33 @@ contract GamesTest is Test {
   }
 
   function test_CreateAndSyncGame() public {
+    vm.startPrank(CREATOR);
     uint256 gameId = games.createGame(PLAYER1, PLAYER2);
     assertEq(gameId, 0);
     games.syncGame(gameId, "test", bytes("0x123"), PLAYER1);
-    (uint256 id, GameUpdate[] memory updates, address player1, address player2) =
-      games.getGame(gameId);
+    (
+      uint256 id,
+      address creator,
+      address player1,
+      address player2,
+      uint8 result,
+      address winner,
+      GameUpdate[] memory updates
+    ) = games.getGame(gameId);
     assertEq(id, gameId);
+    assertEq(creator, CREATOR);
+    assertEq(player1, PLAYER1);
+    assertEq(player2, PLAYER2);
+    assertEq(result, 0);
+    assertEq(winner, address(0));
     assertEq(updates.length, 1);
     assertEq(updates[0].signature, bytes("0x123"));
     assertEq(updates[0].signer, PLAYER1);
-    assertEq(player1, PLAYER1);
-    assertEq(player2, PLAYER2);
+    vm.stopPrank();
   }
 
   function test_CreateAndSyncChessGameState() public {
+    vm.startPrank(CREATOR);
     uint256 gameId = games.createGame(
       0x101a25d0FDC4E9ACa9fA65584A28781046f1BeEe, 0x5dd926aE40ae0deD445603C07D96cFD6B27Fada1
     );
@@ -44,30 +58,53 @@ contract GamesTest is Test {
       bytes("0x101a25d0FDC4E9ACa9fA65584A28781046f1BeEe"),
       address(0x123)
     );
-    (uint256 id, GameUpdate[] memory updates, address player1, address player2) =
-      games.getGame(gameId);
+    vm.stopPrank();
+    (
+      uint256 id,
+      address creator,
+      address player1,
+      address player2,
+      uint8 result,
+      address winner,
+      GameUpdate[] memory updates
+    ) = games.getGame(gameId);
     assertEq(id, gameId);
+    assertEq(creator, CREATOR);
+    assertEq(player1, address(0x101a25d0FDC4E9ACa9fA65584A28781046f1BeEe));
+    assertEq(player2, address(0x5dd926aE40ae0deD445603C07D96cFD6B27Fada1));
+    assertEq(result, 0);
+    assertEq(winner, address(0));
     assertEq(updates.length, 1);
     assertEq(updates[0].signature, bytes("0x101a25d0FDC4E9ACa9fA65584A28781046f1BeEe"));
     assertEq(updates[0].signer, address(0x123));
-    assertEq(player1, address(0x101a25d0FDC4E9ACa9fA65584A28781046f1BeEe));
-    assertEq(player2, address(0x5dd926aE40ae0deD445603C07D96cFD6B27Fada1));
   }
 
   function test_CreateAndSync2ChessGameState() public {
+    vm.startPrank(CREATOR);
     uint256 gameId = games.createGame(
       0x101a25d0FDC4E9ACa9fA65584A28781046f1BeEe, 0x5dd926aE40ae0deD445603C07D96cFD6B27Fada1
     );
+    vm.stopPrank();
     assertEq(gameId, 0);
+    vm.startPrank(0x101a25d0FDC4E9ACa9fA65584A28781046f1BeEe);
     games.syncGame(
       gameId,
       '[White "0x101a25d0FDC4E9ACa9fA65584A28781046f1BeEe"] [Black "0x5dd926aE40ae0deD445603C07D96cFD6B27Fada1"] [Event "7SIOGR"] [Site "Based Chess"] [Date "2025-02-20T20:18:53.754Z"] 1. g4 e5 2. f3 Qh4#',
       hex"9b5f6c7b35719c91cf40187236888cdaf70b97a551cc59d98ae756b10c2717263f382486567f5d6ad7ffb77becaa804a7a79982125da45df25d37ab297b6d9791b",
       address(0x123)
     );
-    (uint256 id, GameUpdate[] memory updates, address player1, address player2) =
-      games.getGame(gameId);
+    vm.stopPrank();
+    (
+      uint256 id,
+      address creator,
+      address player1,
+      address player2,
+      uint8 result,
+      address winner,
+      GameUpdate[] memory updates
+    ) = games.getGame(gameId);
     assertEq(id, gameId);
+    assertEq(creator, CREATOR);
     assertEq(updates.length, 1);
     assertEq(
       updates[0].signature,
@@ -76,5 +113,63 @@ contract GamesTest is Test {
     assertEq(updates[0].signer, address(0x123));
     assertEq(player1, address(0x101a25d0FDC4E9ACa9fA65584A28781046f1BeEe));
     assertEq(player2, address(0x5dd926aE40ae0deD445603C07D96cFD6B27Fada1));
+    assertEq(result, 0);
+    assertEq(winner, address(0));
+  }
+
+  // function test_CreateAndSyncChessGameStateUsingUint8ArrayForSignature() public {
+  //   uint256 gameId = games.createGame(
+  //     0x101a25d0FDC4E9ACa9fA65584A28781046f1BeEe, 0x5dd926aE40ae0deD445603C07D96cFD6B27Fada1
+  //   );
+  //   assertEq(gameId, 0);
+  //   uint8[] memory sig = [72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100, 33];
+  //   games.syncGame(
+  //     gameId,
+  //     '[White "0x101a25d0FDC4E9ACa9fA65584A28781046f1BeEe"] [Black "0x5dd926aE40ae0deD445603C07D96cFD6B27Fada1"] [Event "7SIOGR"] [Site "Based Chess"] [Date "2025-02-20T20:18:53.754Z"] 1. g4 e5 2. f3 Qh4#',
+  //     sig,
+  //     address(0x123)
+  //   );
+  //   (uint256 id, GameUpdate[] memory updates, address player1, address player2) =
+  //     games.getGame(gameId);
+  //   assertEq(id, gameId);
+  //   assertEq(updates.length, 1);
+  //   assertEq(updates[0].signature, sig);
+  //   assertEq(updates[0].signer, address(0x123));
+  //   assertEq(player1, address(0x101a25d0FDC4E9ACa9fA65584A28781046f1BeEe));
+  //   assertEq(player2, address(0x5dd926aE40ae0deD445603C07D96cFD6B27Fada1));
+  // }
+
+  function test_RevertSyncGameNotCreated() public {
+    // uint256 gameId = games.createGame(PLAYER1, PLAYER2);
+    // assertEq(gameId, 0);
+    vm.expectRevert("Game does not exist");
+    games.syncGame(0, "test", bytes("0x123"), PLAYER1);
+  }
+
+  function test_CreateSyncAndVerifyGameWithPlayer1AsWinner() public {
+    vm.startPrank(CREATOR);
+    uint256 gameId = games.createGame(PLAYER1, PLAYER2);
+    assertEq(gameId, 0);
+    games.syncGame(gameId, "test", bytes("0x123"), PLAYER1);
+    games.verifyGameUpdate(gameId, 0, 1, PLAYER1);
+    (
+      uint256 id,
+      address creator,
+      address player1,
+      address player2,
+      uint8 result,
+      address winner,
+      GameUpdate[] memory updates
+    ) = games.getGame(gameId);
+    assertEq(id, gameId);
+    assertEq(creator, CREATOR);
+    assertEq(player1, PLAYER1);
+    assertEq(player2, PLAYER2);
+    assertEq(result, 1);
+    assertEq(winner, PLAYER1);
+    assertEq(updates.length, 1);
+    assertEq(updates[0].signature, bytes("0x123"));
+    assertEq(updates[0].signer, PLAYER1);
+    vm.stopPrank();
   }
 }
